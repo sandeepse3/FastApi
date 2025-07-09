7527cdaa9f76c501e44ee447bbeca0e6ba4ac978
sandeepse3 <sandeepse3@gmail.com>
2025-07-09 15:52:00 +0530

    Section 13: 153 & 154 Alembic Revision Upgrade and Downgrade

--- 3TODOAPP/alembic/versions/2699c1ecc739_create_phone_number_column_in_users_.py
+++ 3TODOAPP/alembic/versions/2699c1ecc739_create_phone_number_column_in_users_.py
@@ -1,18 +1,18 @@
-"""Create phone number for user column
+"""Create phone number column in users table
 
-Revision ID: 5d8db43ffc53
-Revises: 
-Create Date: 2025-07-09 12:26:15.030345
+Revision ID: 2699c1ecc739
+Revises:
+Create Date: 2025-07-09 14:41:02.499397
 
 """
+
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = '5d8db43ffc53'
+revision: str = "2699c1ecc739"
 down_revision: Union[str, Sequence[str], None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -21,11 +21,10 @@ depends_on: Union[str, Sequence[str], None] = None
 def upgrade() -> None:
     """Upgrade schema."""
     op.add_column(
-        'users',
-        sa.Column('phone_number', sa.String(length=15), nullable=True)
+        "users", sa.Column("phone_number", sa.String(length=15), nullable=True)
     )
 
 
 def downgrade() -> None:
     """Downgrade schema."""
-    pass
+    op.drop_column("users", "phone_number")
